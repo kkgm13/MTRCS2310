139ace6652d859b719293351bf7a3e2a4e302a44
sayyid786 <sayyidrasool0@gmail.com>
2017-11-17 10:15:13 +0000

    moved readfile to MTR and removed readfile class

--- src/mtr/MTR.java
+++ src/mtr/MTR.java
@@ -1,11 +1,36 @@
 package mtr;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class MTR {
 
 	private static TUI ui;
 	private static Controller controller;
 	
+	
 	public static void main(String[] args) {
-		ui = new TUI(controller);
+		fileInfo();
+		//ui = new TUI(controller);
 	}
+	
+	private static void fileInfo() {
+		String fileName = "MTRsystem_partial.csv";
+		File file = new File(fileName);
+		try {
+			Scanner inputStream = new Scanner(file);
+			inputStream.useDelimiter(",");
+			while (inputStream.hasNext()) {
+				String data =inputStream.next();
+			//	String[] values = data.split(",");
+				System.out.println(data);
+			}
+			inputStream.close();
+		}catch(FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
 }
