6df7684e5d8f1e8527cd1318c7e33f86f59cbd84
sayyid786 <sayyidrasool0@gmail.com>
2017-11-27 22:35:06 +0000

    Added Line and Station classes to model MTR

--- src/com/aston/compScis/model/Line.java
+++ src/com/aston/compScis/model/Line.java
@@ -1,5 +1,33 @@
 package com.aston.compScis.model;
 
+import java.util.List;
+
 public class Line {
+
+	private String lineName;
+
+	private List<Station> stations;
+
+	public Line(String lineName, List<Station> stations) {
+
+		this.lineName = lineName;
+		this.stations = stations;
+	}
+
+	public String getLineName() {
+		return lineName;
+	}
+
+	public void setLineName(String lineName) {
+		this.lineName = lineName;
+	}
+
+	public List<Station> getStations() {
+		return stations;
+	}
+
+	public void setStations(List<Station> stations) {
+		this.stations = stations;
+	}
 
 }
--- src/com/aston/compScis/model/Station.java
+++ src/com/aston/compScis/model/Station.java
@@ -2,4 +2,18 @@ package com.aston.compScis.model;
 
 public class Station {
 
+	private String stationName;
+
+	public Station() {
+
+	}
+
+	public String getStationName() {
+		return stationName;
+	}
+
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+
 }
