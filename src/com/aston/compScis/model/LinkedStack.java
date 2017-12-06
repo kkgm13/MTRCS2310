618df0f6b815183a8e0f4158e7bf45eaa37dde50
rasools2 <rasools2@aston.ac.uk>
2017-12-06 22:36:02 +0000

    added linkedstack class and the StackADT interface

---
+++ src/com/aston/compScis/model/LinkedStack.java
@@ -0,0 +1,65 @@
+package com.aston.compScis.model;
+
+public class LinkedStack <T> implements StackADT<T> {
+	
+	//DoubleNode used to model top of linkedstack
+	private DoubleNode<T> top;
+	
+	//Count shows elements in linkedstack
+	private int count;
+	
+	//Creates an empty linkedstack 
+	public LinkedStack() {
+		top = null;
+		count = 0;
+	}
+
+	@Override
+	//Pushes element to the head of the linkedstack
+	public void push(T element) {
+		
+		DoubleNode<T> tmp = new DoubleNode<T> (element);
+		tmp.setNext(top);
+		top = tmp;
+		count ++;
+	}
+
+	@Override
+	
+	//Pops element off the head of the linkedstack
+	public T pop() {
+		
+		if (isEmpty()) 
+			throw new IllegalStateException("Stack empty in pop");
+		T result = top.getElement();
+		top = top.getNext();
+		count--;
+		return result;
+	}
+
+	@Override
+	
+	//Returns the top of the linkedstack but does not remove it
+	public T peek() {
+		
+		if (isEmpty()) throw new IllegalStateException("Stack empty in peek");
+		return top.getElement();
+	}
+
+	@Override
+	
+	//Checks if the linkedstack is empty
+	public boolean isEmpty() {
+		
+		return top == null;
+	}
+
+	@Override
+	
+	//Returns the size of the linkedstack
+	public int size() {
+		
+		return count;
+	}
+
+}
---
+++ src/com/aston/compScis/model/StackADT.java
@@ -0,0 +1,40 @@
+package com.aston.compScis.model;
+
+public interface StackADT<T> {
+
+	/**
+	 * Adds one element to the top of the stack.
+	 * 
+	 * @param the
+	 *            element to be added to the stack
+	 */
+	public void push(T element);
+
+	/**
+	 * Removes and returns the top element from the stack.
+	 * 
+	 * @return the element at the top of the stack
+	 */
+	public T pop();
+
+	/**
+	 * Returns the top element of the stack without removing it.
+	 * 
+	 * @return the element at the top of the stack
+	 */
+	public T peek();
+
+	/**
+	 * Returns true if the stack contains no elements and false otherwise.
+	 * 
+	 * @return true if the stack contains no elements
+	 */
+	public boolean isEmpty();
+
+	/**
+	 * Returns the number of elements in the stack.
+	 * 
+	 * @return the number of elements in the stack
+	 */
+	public int size();
+}
